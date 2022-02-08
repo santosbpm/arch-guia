c07f21833e489c1ed49c1bad1ae584649cf272ea
santosbpm <santosbpm.0@gmail.com>
2022-02-08 00:51:58 -0300

    Update base

--- base-uefi.sh
+++ base-uefi.sh
@@ -19,13 +19,11 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
-systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable tlp:
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
