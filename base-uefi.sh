61fcc5dc857bd6dbf7e4fc682905d9f4aa7bf66d
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-04-11 15:05:34 -0300

    update

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,17 +2,18 @@
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+sed -i '178s/.//' /etc/locale.gen
+sed -i '394s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "archbtw" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 archbtw.localdomain archbtw" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion zsh openssh acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables ipset sof-firmware nss-mdns os-prober ntfs-3g curl nvidia nvidia-utils nvidia-settings nvidia-prime
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables ipset sof-firmware nss-mdns os-prober ntfs-3g curl nvidia nvidia-utils nvidia-settings nvidia-prime
 
 grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -24,7 +25,6 @@ systemctl enable avahi-daemon
 systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable firewalld
 systemctl enable acpid
 
 useradd -m -G network,power,users,storage,lp,input,audio,wheel santosbpm
