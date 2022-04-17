7c16d694fce668a656fa14bdd55b1f3fa7151abf
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-04-17 13:28:26 -0300

    Atualização e novas configurações

--- gnome.sh
+++ gnome.sh
@@ -10,11 +10,27 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-sudo pacman -S --noconfirm base-devel xorg xorg-server wayland xorg-xwayland xdg-desktop-portal gdm gnome gnome-tweaks gnome-shell-extensions gnome-bluetooth intel-media-driver flatpak firefox chromium evince gparted fragments vim vjre-openjdk-headlessirtualbox blender gimp mpv smplayer zsh zsh-autosuggestions zsh-syntax-highlighting adwaita-icon-theme papirus-icon-theme jre-openjdk 
+sudo pacman -S --noconfirm base-devel xorg xorg-server wayland xorg-xwayland xdg-desktop-portal gdm gnome gnome-tweaks gnome-shell-extensions intel-media-driver libva-intel-driver libva-utils intel-gpu-tools 
 
-sudo flatpak install -y com.spotify.Client com.google.AndroidStudio org.onlyoffice.desktopeditors com.obsproject.Studio com.microsoft.Teams org.gtk.Gtk3theme.adw-gtk3-dark org.gtk.Gtk3theme.Breeze
+sudo pacman -Rns gnome-books gnome-calendar gnome-characters gnome-clocks gnome-contacts gnome-maps gnome-weather gnome-boxes gnome-music totem gnome-software gnome-photos epiphany inskape
 
-paru install chrome-gnome-shell timeshift timeshift-autosnap 
+sudo pacman -Sy flatpak firefox chromium blender gimp mpv smplayer zsh gnome-themes-extra gnome-icon-theme-extras adwaita-icon-theme adw-gtk-theme volantes-cursors capitaine-cursors papirus-icon-theme jdk-openjdk virtualbox
+
+sudo flatpak install -y com.spotify.Client com.google.AndroidStudio org.onlyoffice.desktopeditors com.obsproject.Studio com.microsoft.Teams
+
+paru install chrome-gnome-shell timeshift virtualbox-ext-oracle
+
+# Configuração zsh
+# chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+# Configuração virtualbox
+sudo gpasswd -a $USER vboxusers
+sudo modprobe vboxguest vboxvideo vboxsf
 
 sudo systemctl enable gdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
