6768ec4cd4541928fbf20089e57e28fc9f9a3f63
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-04-17 13:37:36 -0300

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -13,7 +13,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 archbtw.localdomain archbtw" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables ipset sof-firmware nss-mdns os-prober ntfs-3g curl nvidia nvidia-utils nvidia-settings nvidia-prime
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables ipset sof-firmware nss-mdns os-prober ntfs-3g curl
 
 grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
