30c479380769a1a5345e3f2a2f14684ab6b792b1
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-09-27 19:54:41 -0300

    Update base

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-sed -i '394s/.//' /etc/locale.gen
+sed -i '387s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
@@ -10,7 +10,7 @@ echo "archbtw" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 archbtw.localdomain archbtw" >> /etc/hosts
-echo root:password | chpasswd
+echo root:root | chpasswd
 
 pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables ipset sof-firmware nss-mdns os-prober ntfs-3g curl
 
@@ -26,7 +26,7 @@ systemctl enable fstrim.timer
 systemctl enable acpid
 
 useradd -m -G network,power,users,storage,lp,input,audio,wheel santosbpm
-echo santosbpm:password | chpasswd
+echo santosbpm:santosbpm | chpasswd
 
 echo "santosbpm ALL=(ALL) ALL" >> /etc/sudoers.d/santosbpm
 
