19ad97ae634b1c57e8d9eaca37e8c2fb20768a54
Marcos Paulo <santosbpm.0@gmail.com>
2022-12-03 19:13:34 -0300

    Change and separeted archives

--- gnome.sh
+++ gnome.sh
@@ -9,17 +9,18 @@ sudo pacman -Syyuu
 sudo pacman -S --noconfirm xorg wayland gnome-shell gnome-control-center gnome-tweak-tool gnome-tweaks gnome-shell-extensions gdm libva-intel-driver libva-utils intel-gpu-tools mesa mesa-utils nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime vulkan-intel vulkan-headers vulkan-tools nautilus gnome-terminal file-roller gnome-calculator gnome-system-monitor evince eog gnome-backgrounds gnome-user-share gedit neofetch firefox helvum blender gimp mpv vlc transmission-gtk jdk-openjdk android-tools android-udev flameshot wget powerline
 
 sudo systemctl enable gdm
-
+sudo systemctl enable libvirtd
 sudo pacman -S flatpak
 flatpak install -y spotify androidstudio onlyoffice obsproject teams pycharm-community steam heroic telegram discordapp flatseal
 
+# AUR helper install
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
 paru gnome-browser-connector-git ttf-meslo-nerd-font-powerlevel10k rtl8821cu-morrownr-dkms-git inxi-git
 
-# Configuração zsh
+# ZSH configuration
 sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search zsh-theme-powerlevel10k zsh-completions
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo 'source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
