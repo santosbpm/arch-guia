02703296ce023c5f096778765d6104a03122546e
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-12-03 14:41:19 -0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -6,7 +6,7 @@ sudo hwclock --systohc
 sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syyuu
 
-sudo pacman -S --noconfirm xorg wayland gnome-shell gnome-control-center gnome-tweak-tool gnome-tweaks gnome-shell-extensions gdm libva-intel-driver libva-utils intel-gpu-tools nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime nautilus gnome-terminal file-roller gnome-calculator gnome-system-monitor evince gnome-backgrounds gnome-user-share gedit neofetch firefox helvum blender gimp mpv vlc transmission-gtk zsh zsh-theme-powerlevel10k jdk-openjdk android-tools android-udev flameshot
+sudo pacman -S --noconfirm xorg wayland gnome-shell gnome-control-center gnome-tweak-tool gnome-tweaks gnome-shell-extensions gdm libva-intel-driver libva-utils intel-gpu-tools mesa mesa-utils nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime vulkan-intel vulkan-headers vulkan-tools nautilus gnome-terminal file-roller gnome-calculator gnome-system-monitor evince eog gnome-backgrounds gnome-user-share gedit neofetch firefox helvum blender gimp mpv vlc transmission-gtk jdk-openjdk android-tools android-udev flameshot wget powerline
 
 sudo systemctl enable gdm
 
@@ -17,19 +17,15 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru gnome-browser-connector-git packettracer ttf-meslo-nerd-font-powerlevel10k rtl8821cu-morrownr-dkms-git
+paru gnome-browser-connector-git ttf-meslo-nerd-font-powerlevel10k rtl8821cu-morrownr-dkms-git inxi-git
 
 # Configuração zsh
+sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search zsh-theme-powerlevel10k zsh-completions
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-git clone https://github.com/zsh-users/zsh-completions.git
-fpath=(path/to/zsh-completions/src $fpath)
-rm -f ~/.zcompdump; compinit
-sudo pacman -S zsh-history-substring-search
-echo 'source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh' >> ~/.zshrc
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+echo 'source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh' >> ~/.zshrc
+
 chsh -s $(which zsh)
 
 print "Reinicie o sistema"
