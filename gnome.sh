c11d4d548cfe3ccbf4c7ae0db848d6b38d6baccd
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-12-02 22:38:27 -0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -6,28 +6,30 @@ sudo hwclock --systohc
 sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syyuu
 
-sudo pacman -S --noconfirm xorg xorg-server wayland xorg-xwayland gnome-shell nautilus gnome-terminal gnome-tweak-tool gnome-control-center xdg-user-dirs xdg-desktop-portal xdg-desktop-portal-gtk gdm gnome-tweaks gnome-shell-extensions intel-media-driver libva-intel-driver libva-utils intel-gpu-tools nvidia nvidia-utils nvidia-settings nvidia-prime
-sudo pacman -S neofetch firefox helvum blender gimp mpv smplayer vlc transmission-gtk zsh papirus-icon-theme jdk-openjdk virtualbox android-tools android-udev flameshot
-sudo pacman -S file-roller gnome-calculator gnome-system-monitor evince gnome-backgrounds gnome-remote-desktop gnome-menus gnome-user-share gedit
+sudo pacman -S --noconfirm xorg wayland gnome-shell gnome-control-center gnome-tweak-tool gnome-tweaks gnome-shell-extensions gdm libva-intel-driver libva-utils intel-gpu-tools nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime nautilus gnome-terminal file-roller gnome-calculator gnome-system-monitor evince gnome-backgrounds gnome-user-share gedit neofetch firefox helvum blender gimp mpv vlc transmission-gtk zsh zsh-theme-powerlevel10k jdk-openjdk android-tools android-udev flameshot
+
+sudo systemctl enable gdm
 
 sudo pacman -S flatpak
-sudo flatpak install -y com.spotify.Client com.google.AndroidStudio org.onlyoffice.desktopeditors com.obsproject.Studio com.microsoft.Teams com.jetbrains.PyCharm-Community com.visualstudio.code
+flatpak install -y spotify androidstudio onlyoffice obsproject teams pycharm-community steam heroic telegram discordapp flatseal
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru gnome-browser-connector-git virtualbox-ext-oracle packettracer timeshift timeshift-autosnap
+paru gnome-browser-connector-git packettracer ttf-meslo-nerd-font-powerlevel10k rtl8821cu-morrownr-dkms-git
 
 # Configuração zsh
-# chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo pacman -S powerline zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+git clone https://github.com/zsh-users/zsh-completions.git
+fpath=(path/to/zsh-completions/src $fpath)
+rm -f ~/.zcompdump; compinit
+sudo pacman -S zsh-history-substring-search
+echo 'source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh' >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+chsh -s $(which zsh)
 
-# Configuração virtualbox
-sudo gpasswd -a $USER vboxusers
-sudo modprobe vboxguest vboxvideo vboxsf
-
-sudo systemctl enable gdm
 print "Reinicie o sistema"
