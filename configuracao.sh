1f91bd53b13eb34c083e9ebe9cdaf1ab6dcf1221
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2023-04-13 03:23:33 -0300

    Update configuracao.sh

--- configuracao.sh
+++ configuracao.sh
@@ -34,32 +34,14 @@ passwd santosbpm
 # echo santosbpm:santosbpm | chpasswd
 # echo root:root | chpasswd
 
-mount /dev/mapper/home-crypt /mnt
-
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@games
-btrfs subvolume create /mnt/@VMs
-btrfs subvolume create /mnt/@downloads
-btrfs subvolume create /mnt/@documents
-btrfs subvolume create /mnt/@pictures
-btrfs subvolume create /mnt/@videos
-btrfs subvolume create /mnt/@cache_home
-btrfs subvolume create /mnt/@libvirt_home
-btrfs subvolume create /mnt/@docker_home
-btrfs subvolume create /mnt/@flatpak_home
-
-chattr +C /mnt/@libvirt_home
-chattr +C /mnt/@VMs
-chattr +C /mnt/@docker_home
-
-umount /mnt
-
-sudo -u santosbpm xdg-user-dirs-update
-sudo -u santosbpm mkdir -p ~/{.cache,Games,'VirtualBox VMs'}
-sudo -u santosbpm mkdir -p ~/.local/share/{libvirt,flatpak,docker}
+echo "Veja no readme quais são os comandos deve ser inseridos e saia do usuário"
+su santosbpm
+# xdg-user-dirs-update
+# mkdir -p ~/{.cache,Games,'VirtualBox VMs'}
+# mkdir -p ~/.local/share/{libvirt,flatpak,docker}
 
 mount -o defaults,noatime,compress-force=zstd,subvol=@games /dev/mapper/home-crypt /home/santosbpm/Games
-mount -o defaults,noatime,compress-force=zstd,subvol=@VMs /dev/mapper/home-crypt /home/santosbpm/'VirtualBox VMs'
+mount -o defaults,noatime,compress-force=zstd,subvol=@VMs /dev/mapper/home-crypt /home/santosbpm/VirtualBox\ VMs
 mount -o defaults,noatime,compress-force=zstd,subvol=@downloads /dev/mapper/home-crypt /home/santosbpm/Downloads
 mount -o defaults,noatime,compress-force=zstd,subvol=@documents /dev/mapper/home-crypt /home/santosbpm/Documents
 mount -o defaults,noatime,compress-force=zstd,subvol=@pictures /dev/mapper/home-crypt /home/santosbpm/Pictures
@@ -69,22 +51,24 @@ mount -o defaults,noatime,compress-force=zstd,subvol=@libvirt_home /dev/mapper/h
 mount -o defaults,noatime,compress-force=zstd,subvol=@flatpak_home /dev/mapper/home-crypt /home/santosbpm/.local/share/flatpak
 mount -o defaults,noatime,compress-force=zstd,subvol=@docker_home /dev/mapper/home-crypt /home/santosbpm/.local/share/docker
 
-exit
+chown santosbpm:santosbpm -R /home/santosbpm/
 
-btrfs filesystem mkswapfile --size 16g /mnt/var/swap/swapfile
-swapon /mnt/var/swap/swapfile
+modprobe zram
+echo zstd > /sys/block/zram0/comp_algorithm
+echo 2G > /sys/block/zram0/disksize
+mkswap --label zram0 /dev/zram0
+swapon --priority 100 /dev/zram0
+
+btrfs filesystem mkswapfile --size 16g /var/swap/swapfile
+swapon /var/swap/swapfile
+
+exit
 
 echo rd.luks.uuid=$(lsblk -o UUID /dev/nvme0n1p2 | tail -n 1) rd.luks.name=$(lsblk -o UUID /dev/nvme0n1p2 | tail -n 1)=root rd.luks.options=password-echo=no rootflags=subvol=@ resume=UUID=$(findmnt -no UUID -T /mnt/var/swap/swapfile) resume_offset=$(btrfs inspect-internal map-swapfile -r /mnt/var/swap/swapfile) zswap.enabled=0 rw quiet bgrt_disable nmi_watchdog=0 nowatchdog >> /mnt/etc/kernel/cmdline
 
 echo "vm.swappiness = 10" > /mnt/etc/sysctl.d/99-swappiness.conf
 
-modprobe zram
-echo zstd > /mnt/sys/block/zram0/comp_algorithm
-echo 2G > /mnt/sys/block/zram0/disksize
-mkswap --label zram0 /mnt/dev/zram0
-swapon --priority 100 /mnt/dev/zram0
-
-echo 'home-crypt         UUID='$(lsblk -o UUID /dev/sda1 | tail -n 1)'        none       luks' >> /mnt/etc/crypttab
+echo 'home-crypt         UUID='$(lsblk -o UUID /dev/sda1 | head -n 2 | tail -n 1)'        none       luks' >> /mnt/etc/crypttab
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
